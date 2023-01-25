4ff6e7b2bc462bcf0a82dd948179526751373b35
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-25 19:07:10 +0100

    algorithm challenge solved

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,22 @@
-function divisible(a) {}
+// # Challenge 2
+
+// Implement the function divisible(a) which returns the number of the elements in that array a that are divisible by 2 and greater than 15.
+
+// ## Examples
+
+// divisible([2,3,12,18,42,24]) to return 3  
+// divisible([100,84,37]) to return 2  
+// divisible([5,8,6,12]) to return 0
+
+function divisible(a) {
+    let count = 0;
+
+    for (let i=0; i< a.length; i++){
+        if (a[i] % 2 === 0 && a[i] > 15){
+            count ++;
+        }
+    }
+    return count;
+}
 
 module.exports = divisible
