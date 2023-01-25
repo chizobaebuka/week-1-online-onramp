fb9acb9b01702f8adc903d134d2ca29b8144ebb5
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-01-25 19:00:55 +0100

    tackled algorithm challenge

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,15 @@
-function noOfElements(x, y, z) {}
+// # Challenge 1
+
+// Implement the function noOfElements(x, y, z) which returns the number of elements in that array x, that are greater than or equal to the variable y and less than or equal to the variable z
+
+function noOfElements(x, y, z) {
+    let count = 0;
+    x.forEach((item) => {
+        if (item >= y && item <= z){
+            count ++;
+        }
+    })
+    return count;
+}
 
 module.exports = noOfElements
